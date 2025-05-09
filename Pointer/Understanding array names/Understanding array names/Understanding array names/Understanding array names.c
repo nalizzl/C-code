cf5b1c49d598d72165062c0df7cb6421f8d2549b
nalizzl <3379441786@qq.com>
2025-05-10 00:57:54 +0800

    数组名理解第一次提交

---
+++ Pointer/Understanding array names/Understanding array names/Understanding array names/Understanding array names.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+//不使用数组下标，写一个函数打印数组
+//#include<stdio.h>
+//void Print_arr(int * p)
+//{
+//	int b;
+//	for (b = 0; b < 5; b++)
+//	{
+//		printf("%d ", *(p + b));
+//	}
+//}
+//int main()
+//{
+//	int a, arr[5];
+//	int* pa = arr;
+//	for (a = 0; a < 5; a++)
+//	{
+//		scanf("%d", (pa + a));
+//	}
+//	Print_arr(arr);
+//	return 0;
+//}
