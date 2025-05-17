75b1030d41b03e3e077fa4b36cfc91eafe9a1972
nalizzl <3379441786@qq.com>
2025-05-18 01:12:07 +0800

    strncpy函数测试

---
+++ string function/string function/string function.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char aa[] = "abc\0defghij";
+	char bb[100] = { 0 };
+	strncpy(bb, aa, 7);
+	printf("%s", bb);
+	return 0;
+}
