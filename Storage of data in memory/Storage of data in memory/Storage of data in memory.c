0d2e3fc7f1a9c49a1f7627ccc044d6a903c1ebd7
nalizzl <3379441786@qq.com>
2025-05-18 19:37:06 +0800

    memmove函数的模拟实现，判断大小端字节序，atoi函数初步模拟，数据在内存存储初步试验

---
+++ Storage of data in memory/Storage of data in memory/Storage of data in memory.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 
+//#include<stdio.h>
+//int main()
+//{
+//	char a = 255;
+//	printf("%d\n", a);//-1
+//	return 0;
+//}
+//int main()
+//{
+//    char a[1000] = { 0 };
+//    int i = 0;
+//    for (i = 0; i < 1000; i++)
+//    {
+//        a[i] = -1 - i;
+//        if (a[i] == 0)
+//            break;
+//    }
+//    printf("%d %d", strlen(a), i);//255 255
+//    return 0;
+//}
+
+//写一个函数判断大小端字节序
+//int determine_sys()
+//{
+//	int a = 1;
+//	return (*(char*)&a);
+//}
+//int main()
+//{
+//	int b = determine_sys();
+//	if (b == 1)
+//		printf("小端字节序");
+//	else
+//		printf("大端字节序");
+//	return 0;
+//}
+
+//
--- string function/string function/string function.c
+++ string function/string function/string function.c
@@ -1,11 +1,93 @@
 #define _CRT_SECURE_NO_WARNINGS 
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-	char aa[] = "abc\0defghij";
-	char bb[100] = { 0 };
-	strncpy(bb, aa, 7);
-	printf("%s", bb);
-	return 0;
-}
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char aa[] = "abc\0defghij";
+//	char bb[100] = { 0 };
+//	strncpy(bb, aa, 7);
+//	printf("%s", bb);
+//	return 0;
+//}
+
+//memmove函数的模拟实现
+//#include<assert.h>
+//void * my_memmove(void * dest , void * src, size_t num)
+//{
+//	void* ret = dest;
+//	assert(ret && dest && src);
+//	//dest < src
+//	if (dest < src)
+//	{
+//		while (num--)
+//		{
+//			*(char*)dest = *(char*)src;
+//			dest = ((char*)dest)++;
+//			src = ((char*)src)++;
+//		}
+//	}
+//	//dest >= src
+//	else
+//	{
+//		while (num--)
+//		{
+//			*((char*)dest + num) = *((char*)src + num);
+//		}
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	void* ret = my_memmove(arr + 4, arr + 2, 16);
+//	return 0;
+//}
+
+//atoi函数的学习与模拟实现
+//c++官网范例：
+//#include <stdio.h>      /* printf, fgets */
+//#include <stdlib.h>     /* atoi */
+//
+//int main()
+//{
+//	int i;
+//	char buffer[256];
+//	printf("Enter a number: ");
+//	fgets(buffer, 256, stdin);
+//	i = atoi(buffer);
+//	printf("The value entered is %d. Its double is %d.\n", i, i * 2);
+//	return 0;
+//}
+
+////模拟实现
+//#include<stdio.h>
+//#include<assert.h>
+//int my_atoi(const char* str)
+//{
+//	const char* move = str;
+//	assert(str);
+//	char arr1[20] = {}
+//	while (*move != '\0')
+//	{
+//		if (*move == 32)
+//		{
+//			move++;
+//		}
+//
+//	}
+//}
+//
+//int main()
+//{
+//	my_atoi();
+//	return 0;
+//}
+
+// 
+// 
+//int main()
+//{
+//	char a = 48;
+//	printf("%d", a);
+//	return 0;
+//}
