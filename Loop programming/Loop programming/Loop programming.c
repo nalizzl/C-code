1e1c1b5ffd5516e6f89f6d5c0c5f41168d80b47c
nalizzl <3379441786@qq.com>
2025-05-19 22:58:02 +0800

    猜数字游戏

--- Loop programming/Loop programming/Loop programming.c
+++ Loop programming/Loop programming/Loop programming.c
@@ -27,3 +27,73 @@
 //    }
 //    return 0;
 //}
+
+////猜数字游戏 100到200，六次次数限制
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//
+//
+//void menu()
+//{
+//	printf("*************************\n");
+//	printf("*******0.退出游戏********\n");
+//	printf("*******1.开始游戏********\n");
+//	printf("*************************\n");
+//	printf("请选择：\n");
+//}
+//
+//void game()
+//{
+//	int guess, count = 6;
+//	int ret = 100 + rand() % 101;
+//	while (count)
+//	{
+//		printf("猜一个100到200之间的整数，还有%d次机会，请输入：\n", count);
+//		scanf("%d", &guess);
+//		if (guess < ret)
+//		{
+//			printf("猜小了,请继续：\n");
+//		}
+//		else if (guess > ret)
+//		{
+//			printf("猜大了，请继续：\n");
+//		}
+//		else
+//		{
+//			printf("猜对了，正确结果是：%d\n", ret);
+//			break;
+//		}
+//		count--;
+//	}
+//	if (count != ret && count == 0)
+//	{
+//		printf("剩余机会为%d,正确结果是%d\n", count, ret);
+//	}
+//	else
+//	{
+//		printf("恭喜你，游戏通关！！！\n");
+//	}
+//}
+//
+//int main()
+//{
+//	srand((unsigned int)time(NULL));
+//	int input = 1;
+//	while (input)
+//	{
+//		menu();
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 0:
+//			break;
+//		case 1:
+//			game();
+//			continue;
+//		default:
+//			printf("输入有误，请重新输入\n");
+//		}
+//	}
+//	return 0;
+//}
