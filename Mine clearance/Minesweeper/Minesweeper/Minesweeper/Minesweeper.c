7dd3c1e7d603145c2938d6ea820fe0c9c542f893
nalizzl <3379441786@qq.com>
2025-06-03 23:56:59 +0800

    扫雷游戏实践开始

---
+++ Mine clearance/Minesweeper/Minesweeper/Minesweeper/Minesweeper.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<stdio.h>
+void menu()
+{
+	printf("***********************\n");
+	printf("*****1.enter **********\n");
+	printf("*****0.exit  **********\n");
+	printf("***********************\n");
+}
