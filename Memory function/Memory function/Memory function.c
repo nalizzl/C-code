ac3dca600d78e4e4504cb8bd0fcd9964452ef49f
nalizzl <3379441786@qq.com>
2025-06-09 23:04:45 +0800

    memcpy的模拟实现

--- Memory function/Memory function/Memory function.c
+++ Memory function/Memory function/Memory function.c
@@ -33,45 +33,45 @@
 //	return 0;
 //}
 
-//atoi函数的学习与模拟实现
-//c++官网范例：
-#include <stdio.h>      /* printf, fgets */
-#include <stdlib.h>     /* atoi */
-
-int main()
-{
-	int i;
-	char buffer[256];
-	printf("Enter a number: ");
-	fgets(buffer, 256, stdin);
-	i = atoi(buffer);
-	printf("The value entered is %d. Its double is %d.\n", i, i * 2);
-	return 0;
-}
-
-//模拟实现
-#include<stdio.h>
-#include<assert.h>
-int my_atoi(const char* str)
-{
-	const char* move = str;
-	assert(str);
-	char arr1[20] = {}
-	while (*move != '\0')
-	{
-		if (*move == 32)
-		{
-			move++;
-		}
-
-	}
-}
-
-int main()
-{
-	my_atoi();
-	return 0;
-}
+////atoi函数的学习与模拟实现
+////c++官网范例：
+//#include <stdio.h>      /* printf, fgets */
+//#include <stdlib.h>     /* atoi */
+//
+//int main()
+//{
+//	int i;
+//	char buffer[256];
+//	printf("Enter a number: ");
+//	fgets(buffer, 256, stdin);
+//	i = atoi(buffer);
+//	printf("The value entered is %d. Its double is %d.\n", i, i * 2);
+//	return 0;
+//}
+//
+////模拟实现
+//#include<stdio.h>
+//#include<assert.h>
+//int my_atoi(const char* str)
+//{
+//	const char* move = str;
+//	assert(str);
+//	char arr1[20] = {}
+//	while (*move != '\0')
+//	{
+//		if (*move == 32)
+//		{
+//			move++;
+//		}
+//
+//	}
+//}
+//
+//int main()
+//{
+//	my_atoi();
+//	return 0;
+//}
 
 // 
 // 
@@ -79,5 +79,35 @@ int main()
 //{
 //	char a = 48;
 //	printf("%d", a);
+//	return 0;
+//}
+
+
+//memcpy的模拟实现
+//#include<stdio.h>
+//#include<assert.h>
+//#include<string.h>
+//void* my_memcpy(void* dest, const void* str, size_t num)
+//{
+//	void* ret = dest;
+//	assert(dest && str);
+//	while (num--)
+//	{
+//		*((char*)dest)++ = *((char*)str)++;
+//		/*((char*)dest)++;
+//		((char*)str)++;*/
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	/*char ary1[] = "hello bit";
+//	char ary2[20] = { 0 };
+//	size_t len = strlen(ary1);
+//	void* get = my_memcpy(ary2, ary1, len);
+//	printf(get);*/
+//	int ary3[10] = { 2,3,4,2,4,5,6,5,7,8 };
+//	int ary4[20] = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 };
+//	void* pro = my_memcpy(ary4, ary3, sizeof(int) * 10);
 //	return 0;
 //}
